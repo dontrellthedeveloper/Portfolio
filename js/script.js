3290ebc0a8ffa9c060d8074f34bf31cbb04c87af
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-10-27 14:12:43 -0700

    Added CSS & JS to Title Section

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
 		pagination: false
 	});
 
+	let typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "App Developer."],
+		typeSpeed: 80,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
 });
